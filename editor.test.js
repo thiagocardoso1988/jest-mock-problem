826af238c3255d18a720e9e497deacd27003072d
Rafael Cruz Rubert <rafael@rubert.com.br>
2020-01-24 00:45:43 -0300

    fix test

--- editor.test.js
+++ editor.test.js
@@ -5,17 +5,20 @@
 import Editor from './editor';
 
 describe('test', () => {
-  it('', () => {
+  it('', done => {
     const mockOpenText = jest.fn().mockImplementation(() => console.log('hit mockOpenText'))
     const mockEditorFunctions = {
       openText: mockOpenText
     }
     const subject = new Editor();
     jest.spyOn(subject, 'getEditor').mockImplementation(() => Promise.resolve(mockEditorFunctions))
-    // subject.getEditor().then(console.log)
+    // 
     subject.openEditor({})
 
     console.log(mockOpenText.mock);
-    expect(mockOpenText).toHaveBeenCalled();
+    return subject.getEditor().then( () => {
+      expect(mockOpenText).toHaveBeenCalled();
+      done();
+    });
   });
 })
